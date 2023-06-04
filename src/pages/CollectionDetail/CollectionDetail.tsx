5fa6ff8dfdd9defc9a715edafc5a6c7c321168a3
Jerry Pao <kyjerry0424@gmail.com>
2023-06-04 19:48:36 -0400

    define collection component

---
+++ src/pages/CollectionDetail/CollectionDetail.tsx
@@ -0,0 +1,18 @@
+//css
+import styles from './CollectionDetail.module.css'
+
+//types
+import { Collection } from '../../types/models'
+
+
+import React from 'react'
+
+const CollectionDetail = () => {
+  return (
+    <main className={styles.container}>
+        CollectionDetail
+    </main>
+  )
+}
+
+export default CollectionDetail
